004948e4d3d4704dd62f50d2a38e88e703b802b1
BeautifulBabe-MTM <86706552+BeautifulBabe-MTM@users.noreply.github.com>
2021-07-07 14:50:38 +0300

    add project with all commits

---
+++ NoteGUI/NoteGUI/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+using System;
+using System.Globalization;
+
+namespace NoteGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+
+        System.ComponentModel.ComponentResourceManager resources;
+        private void InitializeComponent()
+        {
+            resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.bottomPanel = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.languageToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.arToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.esToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ruToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.enUSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bottomPanel.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // searchTextBox
+            // 
+            resources.ApplyResources(this.searchTextBox, "searchTextBox");
+            this.searchTextBox.Name = "searchTextBox";
+            // 
+            // searchButton
+            // 
+            resources.ApplyResources(this.searchButton, "searchButton");
+            this.searchButton.Name = "searchButton";
+            // 
+            // bottomPanel
+            // 
+            this.bottomPanel.BackColor = System.Drawing.Color.Gray;
+            this.bottomPanel.Controls.Add(this.button2);
+            this.bottomPanel.Controls.Add(this.button1);
+            resources.ApplyResources(this.bottomPanel, "bottomPanel");
+            this.bottomPanel.Name = "bottomPanel";
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.languageToolStripMenuItem1});
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // languageToolStripMenuItem1
+            // 
+            this.languageToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arToolStripMenuItem1,
+            this.esToolStripMenuItem1,
+            this.ruToolStripMenuItem1,
+            this.enUSToolStripMenuItem});
+            this.languageToolStripMenuItem1.Name = "languageToolStripMenuItem1";
+            resources.ApplyResources(this.languageToolStripMenuItem1, "languageToolStripMenuItem1");
+            // 
+            // arToolStripMenuItem1
+            // 
+            this.arToolStripMenuItem1.Name = "arToolStripMenuItem1";
+            this.arToolStripMenuItem1.Click += LanguageToolStripMenuItem_Click;
+            resources.ApplyResources(this.arToolStripMenuItem1, "arToolStripMenuItem1");
+            // 
+            // esToolStripMenuItem1
+            // 
+            this.esToolStripMenuItem1.Name = "esToolStripMenuItem1";
+            this.esToolStripMenuItem1.Click += LanguageToolStripMenuItem_Click;
+            resources.ApplyResources(this.esToolStripMenuItem1, "esToolStripMenuItem1");
+            //
+            // ruToolStripMenuItem1
+            // 
+            this.ruToolStripMenuItem1.Name = "ruToolStripMenuItem1";
+            this.ruToolStripMenuItem1.Click += LanguageToolStripMenuItem_Click;
+            resources.ApplyResources(this.ruToolStripMenuItem1, "ruToolStripMenuItem1");
+            // 
+            // enUSToolStripMenuItem
+            // 
+            this.enUSToolStripMenuItem.Name = "enUSToolStripMenuItem";
+            this.enUSToolStripMenuItem.Click += LanguageToolStripMenuItem_Click;
+            resources.ApplyResources(this.enUSToolStripMenuItem, "enUSToolStripMenuItem");
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.bottomPanel);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.bottomPanel.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SettingsForm settings = new SettingsForm();
+            settings.ShowDialog();
+        }
+
+        private void LanguageToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            CultureInfo ci = new CultureInfo((sender as ToolStripMenuItem).Text);
+            foreach (Control c in this.Controls)
+                resources.ApplyResources(c, c.Name, ci);
+            foreach (ToolStripItem c in menuStrip1.Items)
+            {
+                foreach (ToolStripItem item in languageToolStripMenuItem1.DropDownItems)
+                    resources.ApplyResources(item, item.Name, ci);
+                resources.ApplyResources(c, c.Name, ci);
+            }
+        }
+
+        private Panel bottomPanel;
+        private Button searchButton;
+        private TextBox searchTextBox;
+
+
+        #endregion
+        private MenuStrip menuStrip1;
+        private Button button1;
+        private Button button2;
+        private ToolStripMenuItem languageToolStripMenuItem1;
+        private ToolStripMenuItem arToolStripMenuItem1;
+        private ToolStripMenuItem esToolStripMenuItem1;
+        private ToolStripMenuItem ruToolStripMenuItem1;
+        private ToolStripMenuItem enUSToolStripMenuItem;
+    }
+}
+
---
+++ NoteGUI/NoteGUI/SettingsForm.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+using System.Drawing;
+
+namespace NoteGUI
+{
+    partial class SettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Change Back Color";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Set Back Image";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.DarkGray;
+            this.label1.Location = new System.Drawing.Point(-3, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 2);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.DarkGray;
+            this.label2.Location = new System.Drawing.Point(141, -1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(2, 78);
+            this.label2.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Location = new System.Drawing.Point(142, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(143, 79);
+            this.panel1.TabIndex = 4;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 15);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(110, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Transparent Back";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 47);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(83, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Visible Back";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.DarkGray;
+            this.label3.Location = new System.Drawing.Point(-2, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 2);
+            this.label3.TabIndex = 5;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(10, 133);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(146, 13);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://github.com/STD-Blue";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Our Project On GitHub";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(209, 128);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Done";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(104, 80);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "About Us";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // SettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 155);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "SettingsForm";
+            this.Text = "Settings";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
